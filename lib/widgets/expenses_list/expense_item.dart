0b3c3a5a32aa19ab77aec2980f4ff7cdbf143a5a
arkteck <arkteck@gmail.com>
2024-07-12 09:46:41 -0400

    add icons, format date

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -14,7 +14,25 @@ class ExpenseItem extends StatelessWidget {
           horizontal: 20,
           vertical: 16,
         ),
-        child: Text(expense.title),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
