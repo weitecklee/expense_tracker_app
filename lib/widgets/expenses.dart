e2dc699c7e5874fb2a6c4c0f31905a2fda5b0ad2
arkteck <arkteck@gmail.com>
2024-07-12 09:56:15 -0400

    app bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,6 +28,15 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker App'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text('CHART'),
