1c964d3392edb3217b9596847028a7a8d478576a
arkteck <arkteck@gmail.com>
2024-07-12 14:02:49 -0400

    prelim theming

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,35 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
